da10f9454f415f30aef48b0c562ea72261b1b995
Louis <fantasim.dev@gmail.com>
2020-08-01 15:48:01 +0200

    clean

--- src/lib.ts
+++
@@ -1,9 +0,0 @@
-export const hash = (s: string): number => {
-    let hash = 0;
-    for (var i = 0; i < s.length; i++) {
-        const char = s.charCodeAt(i);
-        hash = ((hash<<5)-hash)+char;
-        hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
-}
