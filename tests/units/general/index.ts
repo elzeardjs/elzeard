5e8e73fa806819e1cf5fb7512622aec33aa3427d
Fantasim <9706112+Fantasim@users.noreply.github.com>
2021-06-24 07:21:53 +0300

    Update index.ts

--- tests/units/general/index.ts
+++ tests/units/general/index.ts
@@ -4,7 +4,7 @@ const main = async () => {
     config.setMySQLConfig({
         host: '185.212.226.103',
         user: 'root',
-        password: 'fH95b88kCE!',
+        password: '',
         database: 'inspirationeum'
     })
     config.setHistoryDirPath('./history')
